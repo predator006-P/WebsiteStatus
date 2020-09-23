141fa884a9fa5ba3369a6006a566671876fc2311
Peti <predator006@freemail.hu>
2020-09-24 01:00:28 +0200

    second commit

--- Worker.cs
+++ Worker.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
@@ -11,18 +12,31 @@ namespace WebsiteStatus
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private HttpClient client;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
+        }
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            client = new HttpClient();
+            return base.StartAsync(cancellationToken);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(5000, stoppingToken);
+                var result = await client.GetAsync("https://www.iamtimcorey.com");
+                if (result.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation("The website is up. Status code {StatusCode}", result.StatusCode);
+                }
+                else
+                {
+                    _logger.LogError("The website is down. Status code {StatusCode}", result.StatusCode);
+                }
             }
         }
     }
