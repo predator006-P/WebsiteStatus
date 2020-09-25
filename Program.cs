4631b136c4c5e3da6ba54c3a79c3621d4adb4125
Peter-Szilard Nemeth <Peter-Szilard.Nemeth@heitec.hu>
2020-09-25 12:25:52 +0200

    final commit

--- Program.cs
+++ Program.cs
@@ -4,21 +4,49 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace WebsiteStatus
 {
     public class Program
     {
         public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
+        {   
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.File(@"D:\Self_development\WebsiteStatus\LogFile.txt")
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting up the service");
+                CreateHostBuilder(args).Build().Run();
+                return;
+            }
+            catch(Exception ex)
+            {
+                Log.Fatal(ex, "There was a problem starting the service");
+                return;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+            .UseWindowsService()
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
-                });
+                })
+                .UseSerilog();
+        }
     }
 }
